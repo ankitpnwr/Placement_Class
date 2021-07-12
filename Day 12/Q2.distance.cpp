db822f36bd12977b275cc693f6acc4d4322a4977
ankitpanwar <ap11821141@gmail.com>
2021-07-12 22:18:28 +0530

    adding files of day12

---
+++ Day 12/Q2.distance.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> shortestToChar(string s, char c) {
+
+		vector<int> ans(s.length());
+
+		vector<int> temp;
+		for (int i = 0 ; i < s.length() ; i++) {
+			if (s[i] == c) temp.push_back(i);
+		}
+
+		for (int i = 0 ; i < s.length() ; i++) {
+			int mn = INT_MAX;
+			for (int j = 0 ; j < temp.size() ; j++) {
+				int dis = abs(i - temp[j]);
+				if (dis < mn) mn = dis;
+				else break;
+			}
+			ans[i] = mn;
+		}
+
+		return ans;
+	}
+};
---
+++ Day 12/Q3.mostComman.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	string mostCommonWord(string paragraph, vector<string>& banned) {
+
+		transform(paragraph.begin(), paragraph.end(), paragraph.begin(), ::tolower);
+		for (int i = 0 ; i < paragraph.length() ; i++) {
+			if (!isalpha(paragraph[i])) paragraph[i] = ' ';
+		}
+
+		// vector<string> s;
+		stringstream ss(paragraph);
+
+		string word;
+		unordered_map<string, int> mp;
+
+		while (ss >> word) {
+			mp[word]++;
+		}
+
+		for (int i = 0 ; i < banned.size() ; i++) {
+			mp.erase(banned[i]);
+		}
+
+		int mx = INT_MIN;
+		string temp;
+		for (auto x : mp) {
+			if (x.second > mx) mx = x.second, temp = x.first;
+		}
+
+		return temp;
+
+	}
+};
